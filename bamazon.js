ac642be4c32cd03a496d2a7cbd398b38cd04f7d0
Mosbycr <mosbycr@gmail.com>
2019-04-28 19:48:21 -0400

    inquirer npm connected, inquirer buyProduct function written, question and mysql queries working, checks correctly if have enough stock for id of product wish to purchase

--- bamazon.js
+++ bamazon.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,9 +17,51 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  console.log("you are connected");
   buyProduct();
 });
 
-function buyProduct(){
-    
+function buyProduct() {
+  connection.query("SELECT id, product_name, price FROM products", function(err, results) {
+    if (err) throw err;
+    //console.log(results);
+    //  will need to show this!! - find prettier way to show
+
+    inquirer
+      .prompt([
+        {
+          name: "id",
+          type: "input",
+          message: "What is the id of the product you wish to buy?"
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "How many would you like to purchase?"
+        }
+      ])
+      .then(function(answer) {
+        //console.log(answer);
+         connection.query(`SELECT * FROM products WHERE id = ${answer.id}`, function(err, results) {
+           if (err) throw err;
+          console.log(results);
+          console.log(parseInt(answer.quantity));
+          console.log(results[0].stock_quantity);
+
+          if(parseInt(answer.quantity) <= results[0].stock_quantity){
+            console.log("we have enough");
+          } else {
+            console.log('we do not have enough');
+          }
+         });
+      });
+  });
+
+  
+
+  
+  //   var query = "SELECT id FROM products";
+  //     connection.query(query, function(err, res) {
+  //
+  //    ;
 }
