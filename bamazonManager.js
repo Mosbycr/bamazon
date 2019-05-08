4e4fd056a7312cb52a3e2499f57d12df3ca8120d
Mosbycr <46547100+Mosbycr@users.noreply.github.com>
2019-05-07 20:08:17 -0400

    Update bamazonManager.js

--- bamazonManager.js
+++ bamazonManager.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   // Your password for MySQL goes here
-  password: "3ThreeBelieve!",
+  password: "",
   database: "bamazon"
 });
 
